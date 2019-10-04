3bd89fab92b99240dccacecedaa8772794516044
Christopher Lebovitz <w10012072@usm.edu>
2019-10-04 15:21:44 -0500

    created the visual representation of Board

--- chrisSandBox.py
+++ chrisSandBox.py
@@ -1,17 +1,71 @@
-import brain
-import tree
-import copy
-class Node: 
-    def __init__(self,key): 
-        self.left = None
-        self.right = None
-        self.val = key 
+import math
 
-currentState = tree.Node(0, copy.deepcopy(mainBoard))
-currentState.nextTurns = [4,3,5,123,125,1,5,8,1]
-turn = 0
-# create root 
+import pygame
+from pygame.display import set_mode
+from pygame.draw import rect
+from pygame.rect import Rect
 
 
-brain.alphaBeta(currentState, turn, 0)
-tree.printTree
+
+squareCenters = []
+BoardWidth = 640
+BoardHeight = 640
+screen = pygame.display.set_mode((900,700))
+light_brown = (251, 196, 117)
+gray = (100, 100, 100)
+violet = (238, 130, 238)
+dark_brown = (139, 69, 0)
+colors = [dark_brown, light_brown]
+
+def draw_board(the_board):
+    """ Draw a chess board with queens, as determined by the the_board. """
+
+    pygame.init()
+    colors = [light_brown, dark_brown]    # Set up colors [red, black]
+
+    n = len(the_board)         # This is an NxN chess board.
+    surface_sz = 480           # Proposed physical surface size.
+    sq_sz = surface_sz // n    # sq_sz is length of a square.
+    surface_sz = n * sq_sz     # Adjust to exactly fit n squares.
+
+    # Create the surface of (width, height), and its window.
+    surface = pygame.display.set_mode((surface_sz, surface_sz))
+
+    Pawn = pygame.image.load("BlackPawn.png")
+
+    # Use an extra offset to centre the ball in its square.
+    # If the square is too small, offset becomes negative,
+    #   but it will still be centered :-)
+    Pawn_offset = (sq_sz-Pawn.get_width()) // 2
+
+    while True:
+
+        # Look for an event from keyboard, mouse, etc.
+        ev = pygame.event.poll()
+        if ev.type == pygame.QUIT:
+            break;
+
+        # Draw a fresh background (a blank chess board)
+        for row in range(n):           # Draw each row of the board.
+            c_indx = row % 2           # Alternate starting color
+            for col in range(n):       # Run through cols drawing squares
+                the_square = (col*sq_sz, row*sq_sz, sq_sz, sq_sz)
+                surface.fill(colors[c_indx], the_square)
+                # Now flip the color index for the next square
+                c_indx = (c_indx + 1) % 2
+
+        # Now that squares are drawn, draw the queens.
+        for (col, row) in enumerate(the_board):
+          surface.blit(Pawn,
+                   (col*sq_sz+Pawn_offset,row*sq_sz+Pawn_offset))
+
+        pygame.display.flip()
+
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    draw_board([1,1,1,1,1,1,1,])    # 7 x 7 to test window size
+    draw_board([6, 4, 2, 0, 5, 7, 1, 3])
+    draw_board([9, 6, 0, 3, 10, 7, 2, 4, 12, 8, 11, 5, 1])  # 13 x 13
+    draw_board([11, 4, 8, 12, 2, 7, 3, 15, 0, 14, 10, 6, 13, 1, 5, 9])
