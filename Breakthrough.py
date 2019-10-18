3ae8466302126bb1585402d66bcd7e27aa6cb909
SpiffyGeek <52330244+SpiffyGeek@users.noreply.github.com>
2019-10-18 17:05:57 -0500

    Saving more comment clarifications before submitting assignment, more changes are probably coming to make the code fit the report more easily (WORK IN PROGRESS)

--- Breakthrough.py
+++ Breakthrough.py
@@ -31,7 +31,7 @@ display.startScreen()
 
 
 #Game 1
-match ="Minimax Offensive Heuristic 1 VS AlphaBeta Offensive Heuristic 1"
+match = "Minimax Offensive Heuristic 1 VS AlphaBeta Offensive Heuristic 1"
 game1board = boardV2.board(b, b, "[]", legalMoves)
 player1 = player.player("White", "WW", 0, 1, offensiveStrategy1, False, game1board)
 player2 = player.player("Black", "BB", 1, 1, offensiveStrategy1, True, game1board)
@@ -39,7 +39,7 @@ boardV2.runGame(player1, player2, game1board,match)
 
 
 #Game 2: (all following games use only AlphaBeta, not Minimax)
-match="Offensive Heuristic 2 VS Defensive Heuristic 1"
+match = "Offensive Heuristic 2 VS Defensive Heuristic 1"
 game2board = boardV2.board(b, b, "[]", legalMoves)
 player3 = player.player("White", "WW", 0, 1, offensiveStrategy2, True, game2board)
 player4 = player.player("Black", "BB", 1, 1, defensiveStrategy1, True, game2board)
@@ -48,7 +48,7 @@ boardV2.runGame(player3, player4, game2board,match)
 
 
 #Game 3:
-match="Defensive Heuristic 2 VS Offensive Heuristic 1"
+match = "Defensive Heuristic 2 VS Offensive Heuristic 1"
 game3board = boardV2.board(b, b, "[]", legalMoves)
 player5 = player.player("White", "WW", 0, 1, defensiveStrategy2, True, game3board)
 player6 = player.player("Black", "BB", 1, 1, offensiveStrategy1, True, game3board)
@@ -57,7 +57,7 @@ boardV2.runGame(player5, player6, game3board,match)
 
 
 #Game 4:
-match="Offensive Heuristic 2 VS Offensive Heuristic 1"
+match = "Offensive Heuristic 2 VS Offensive Heuristic 1"
 game4board = boardV2.board(b, b, "[]", legalMoves)
 player7 = player.player("White", "WW", 0, 1, offensiveStrategy2, True, game4board)
 player8 = player.player("Black", "BB", 1, 1, offensiveStrategy1, True, game4board)
--- brainV2.py
+++ brainV2.py
@@ -23,25 +23,23 @@ def getPossibleStates(fPlayer, fState, fCurrentTurn, fDepth):
     #define tree to be returned at end of function
     newStates = []
 
-    #get current player
-    #if fCurrentTurn % 2 == fPlayer.turn:
     #for each possible move from current states
     for i in range(fPlayer.board.row):
-        #debugging, this loop takes a long time to complete so this is to know it's in progress
-        #print("thinking...")
         for j in range(fPlayer.board.col):
             
             #if a space contains a piece of the current player
             if fState.state[i][j] == fPlayer.token: 
+
                 #check all valid moves that player can make from the current state
                 for move in fPlayer.board.moves:
                     tempState = copy.deepcopy(fState.state)
+
+                    #if a valid move can be made, add that move to the list of possible next turns
                     if(boardV2.makeMove(tempState, fPlayer, int(i), int(j), move)):
-                        #if producing a terminal node, calculate heuristic
                         h = 0
+                        #if producing a terminal node, calculate heuristic
                         if(fDepth == searchDepth-1):
                             h = player.highHeuristic(fPlayer, copy.deepcopy(tempState))
-                        #if a valid move can be made, add that move to the list of possible next turns
                         newStates.append(treeV2.Node(h, copy.deepcopy(tempState))) 
                         boardV2.nodeCounter += 1
                         #board.printBoard(tempState)
@@ -50,7 +48,6 @@ def getPossibleStates(fPlayer, fState, fCurrentTurn, fDepth):
     #recurse to the appropriate depth
     if fDepth < searchDepth-1:
         for b in range(len(newStates)):
-            #print("HEllo World")
             newStates[b].nextTurns = getPossibleStates(fPlayer.opponent, newStates[b], fCurrentTurn+1, fDepth+1)
     
     #return the list of possible states to the appropriate depth back to 
@@ -62,12 +59,6 @@ def getPossibleStates(fPlayer, fState, fCurrentTurn, fDepth):
 
 #minimax
 def minimax(fNode, fDepth):
-
-    """
-    start = 0
-    end = 0
-    start=timer()
-    """
 
     #all trees should be built to global searchDepth variable
     #   so we know at that depth is the terminal nodes
@@ -94,7 +85,6 @@ def minimax(fNode, fDepth):
     elif fDepth % 2 == 1:
         fNode.heuristic = treeV2.maxHeuristic(fNode)
 
-    #print("The amout of time that minimax took:", timedelta(seconds = end - start))
 #end minimax
 
 
